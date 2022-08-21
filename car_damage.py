c6f189fc9b3582f5c73d23e67ccf593bce70cd67
Carlo Cochran <carlo.cochran@gmail.com>
2022-08-21 00:44:16 -0400

    updates

--- car_damage.py
+++ car_damage.py
@@ -14,9 +14,11 @@ DATA_PATH = './preprocessed/'
 TRAINING_PCT = 0.7
 VALIDATION_PCT = 0.15
 TEST_PCT = 0.15
-BATCH_SIZE = 64
-LEARNING_RATE = 0.001
-MOMENTUM = 0.9
+BATCH_SIZE = 128
+# LEARNING_RATE = 0.001
+LEARNING_RATE_RANGE = [0.01, 0.001]
+# MOMENTUM = 0.9
+MOMENTUM_RANGE = [0.5, 0.9]
 EPOCH = 2
 NUM_CLASSES = 8
 
@@ -47,39 +49,52 @@ df = pd.read_csv(DATA_PATH + 'index.csv')
 train, validate, test = np.split(df.sample(frac=1),
                                  [int(TRAINING_PCT*len(df)), int((TRAINING_PCT+VALIDATION_PCT)*len(df))])
 
-device = torch.device('cuda') if torch.cuda.is_available() else torch.device('cpu')
-net = torchvision.models.resnet18()
-net.fc = nn.Linear(512, NUM_CLASSES)
-net.to(device)
-criterion = nn.CrossEntropyLoss()
-optimizer = optim.SGD(net.parameters(), lr=LEARNING_RATE, momentum=MOMENTUM)
+for LEARNING_RATE in LEARNING_RATE_RANGE:
+    for MOMENTUM in MOMENTUM_RANGE:
+        print('Learning Rate: {:f}, Momentum: {:f}'.format(LEARNING_RATE, MOMENTUM))
+        net = torchvision.models.resnet18()
+        net.fc = nn.Linear(512, NUM_CLASSES)
+        if torch.cuda.is_available():
+            net = net.cuda()
+        criterion = nn.CrossEntropyLoss()
+        optimizer = optim.SGD(net.parameters(), lr=LEARNING_RATE, momentum=MOMENTUM)
 
-trainset = CustomImageDataset(train)
-trainloader = torch.utils.data.DataLoader(trainset, batch_size=BATCH_SIZE, shuffle=False)
+        trainset = CustomImageDataset(train)
+        trainloader = torch.utils.data.DataLoader(trainset, batch_size=BATCH_SIZE, shuffle=False)
 
-validateset = CustomImageDataset(validate)
-validateloader = torch.utils.data.DataLoader(validateset, batch_size=BATCH_SIZE, shuffle=False)
+        validateset = CustomImageDataset(validate)
+        validateloader = torch.utils.data.DataLoader(validateset, batch_size=BATCH_SIZE, shuffle=False)
 
-for epoch in range(EPOCH):
-    running_loss = 0.0
-    validation_acc = 0
-    for i, data in enumerate(trainloader, 0):
-        inputs, labels = data
-        inputs, labels = inputs.to(device), labels.to(device)
-        optimizer.zero_grad()
-        outputs = net(inputs)
-        loss = criterion(outputs, labels)
-        loss.backward()
-        optimizer.step()
-        running_loss += loss.item()
-        print(f'[{epoch + 1}, {i + 1:5d}] loss: {running_loss}')
+        for epoch in range(EPOCH):
+            training_running_loss = 0.0
+            validation_acc = 0
+            for i, data in enumerate(trainloader, 0):
+                inputs, labels = data
+                if torch.cuda.is_available():
+                    inputs = inputs.cuda()
+                    labels = labels.cuda()
+                optimizer.zero_grad()
+                outputs = net(inputs)
+                training_loss = criterion(outputs, labels)
+                training_loss.backward()
+                optimizer.step()
+                training_running_loss += training_loss.item()
+                print(f'[{epoch + 1}, {i + 1:5d}] training loss: {training_running_loss}')
 
-    for i, data in enumerate(validateloader, 0):
-        inputs, labels = data
-        inputs, labels = inputs.to(device), labels.to(device)
-        outputs = net(inputs)
-        _, predicted = torch.max(outputs, 1)
-        validation_acc += torch.sum(predicted == labels)
+            validation_running_loss = 0.0
+            for i, data in enumerate(validateloader, 0):
+                inputs, labels = data
+                if torch.cuda.is_available():
+                    inputs = inputs.cuda()
+                    labels = labels.cuda()
+                net.eval()
+                outputs = net(inputs)
+                validation_loss = criterion(outputs, labels)
+                validation_running_loss += validation_loss.item()
+                print(f'[{epoch + 1}, {i + 1:5d}] validation loss: {validation_running_loss}')
+                _, predicted = torch.max(outputs, 1)
+                validation_acc += torch.sum(predicted == labels)
 
-    print('Epoch: {:d}, Learning Rate: {:f}, Momentum: {:f}, Accuracy: {:f}'
-          .format(epoch+1, LEARNING_RATE, MOMENTUM, (validation_acc/len(validate.index)).float()))
+            print('Epoch: {:d}, Learning Rate: {:f}, Momentum: {:f}, Accuracy: {:f}, Training Loss: {:f}, Validation Loss: {:f}'
+                  .format(epoch+1, LEARNING_RATE, MOMENTUM, (validation_acc/len(validate.index)).item(),
+                  training_running_loss, validation_running_loss))
